401181557f3c4e84f3400572ec1b36ad0072cbd7
IEM KwangXrak <gyan.acomt@gmail.com>
2022-03-23 01:44:27 +0800

    Update Algorithm
    - 參攷切韻拼音旳代碼重寫了算灋
    - 不再默訒將東韻視爲合口
    - 修攺了三B介音旳表睍，睍在韻核爲歬元音旹保持不變，其他情況下變爲「rv」「ru」
    - 蒸韻韻核默訒値攺爲「i」
    - 增加了選項
    - 更新了說眀和字表

--- prengQvm.js
+++ prengQvm.js
@@ -1,197 +1,153 @@
-/* Hraek自用拼音方案
+/* hraek自用拼音
  *
- * https://zhuanlan.zhihu.com/p/483984744
+ * 參攷：
+ *   切韻拼音
+ *   https://zhuanlan.zhihu.com/p/478751152
+ *   原作者 @unt
  *
- * @author Hraek
- * 
- * 代碼參考：
- * unt切韻擬音J https://phesoca.com/aws/309/
+ * https://github.com/IemKwangqHraek/prengQvm
+ * @author hraek
  */
 
 const is = (x) => 音韻地位.屬於(x);
 
-// 選項
 if (!音韻地位) return [
-  ['支宵侵韻重紐爲AC類對立' , true ],
+  ['支B寫作C類',      true ], // 例: 糜(靡爲切) 開啓: mve,    關閉: mrie;
+                              //     爲(薳支切) 開啓: ue,     關閉: rye.
+  ['侵B寫作C類',      true ], // 例: 琴(巨金切) 開啓: gvm,    關閉: grim.
+  ['宵B寫作C類',      true ], // 例: 囂(許嬌切) 開啓: hvew,   關閉: hriew.
+  ['莊組庚韻歸二等',  true ], // 例: 生(所庚切) 開啓: sraeng, 關閉: sriaeng.
+  ['蒸韻韻核視作i',   true ], // 例: 蒸(煑仍切) 開啓: tjing,  關閉: tjvng.
+  ['東韻三A視作合口', false], // 例: 終(職戎切) 開啓: tyung,  關閉: tjung.
+  ['尤韻三A視作合口', false], // 例: 猷(以周切) 開啓: yu,     關閉: iu.
 ];
 
-if (音韻地位.描述 === '云開三眞平') return 'ghin'; // 礥小韻
+// 特字特辦，避免麻煩
+if (音韻地位.描述 === '云開三眞平') return 'ghen'; // 礥小韻
 if (音韻地位.描述 === '匣合一灰上' && '倄侑'.includes(字頭)) return 'uoiq'; // 倄小韻
 if (音韻地位.描述 === '並三陽上') return 'biangq'; // 𩦠小韻
+if (音韻地位.描述 === '來開二庚上') return 'laengq'; // 冷小韻
+if (音韻地位.描述 === '知開二庚上' && 字頭 === '打') return 'taengq'; // 打
+if (音韻地位.描述 === '知開三麻平') return 'tiae'; // 爹
+if (字頭 === '𠁫') return 'khriw'; // 𠁫
 
-// 音韻地位設置
-const is脣化 = is('東韻 三等 或 合口 或 虞鍾韻');
-const is三B = (is('重紐B類 或 庚臻韻')                  ||
-              is('溪母 幽韻 平聲')                      || // “𠁫”小韻歸三 B
-              is('云母 脂支祭眞臻仙宵麻庚清蒸幽侵鹽韻') || // 云母前元音韻歸三 B
-              '抑𡊁烋'.includes(字頭)                   || // 蒸韻“抑𡊁”二字、幽韻“烋”字歸三 B
-              (字頭 == '揭' && is('見母 仙韻'))         || // “孑”小韻的“揭”字歸三 B
-              (is('蒸幽韻') && is('幫組 或 合口'))) &&
-              !(選項.支宵侵韻重紐爲AC類對立 && is('支宵侵韻 重紐B類 或 云母 支宵侵韻'));
-const is銳前 = is('端精組 或 來母 一二四等');
-const is銳後 = is('知莊章組 或 日以母'); 
-const is銳 = is銳前 || is銳後 || is('來母');  
+const is圓脣 = is('合口 或 虞鍾韻')                 ||
+               (is('東韻') && 選項.東韻三A視作合口) ||
+               (is('尤韻') && 選項.尤韻三A視作合口);
+
+const is非三三等 = is('莊組 庚韻 三等') && 選項.莊組庚韻歸二等;
+
+const is強制三A = is('章組 或 日以母');
+
+const is翹舌聲母 = is('知莊組');
 
 function get聲母() {
-  switch (音韻地位.母) {
-    case '幫': return 'p';
-    case '滂': return 'ph';
-    case '並': return 'b';
-    case '明': return 'm';
-    case '端': return 't';
-    case '透': return 'th';
-    case '定': return 'd';
-    case '泥': return 'n';
-    case '知': return 'tr';
-    case '徹': return 'thr';
-    case '澄': return 'dr';
-    case '孃': return 'nr';
-    case '精': return 'ts';
-    case '清': return 'tsh';
-    case '從': return 'dz';
-    case '心': return 's';
-    case '邪': return 'z';
-    case '莊': return 'tsr';
-    case '初': return 'tshr';
-    case '崇': return 'dzr';
-    case '生': return 'sr';
-    case '俟': return 'zr';
-    case '章': return is脣化 ? 'ty'  : 'tj';
-    case '昌': return is脣化 ? 'thy' : 'thj';
-    case '常': return is脣化 ? 'dy'  : 'dj';
-    case '書': return is脣化 ? 'hy'  : 'hj';
-    case '船': return is脣化 ? 'ghy' : 'ghj';
-    case '日': return is脣化 ? 'ny'  : 'nj';
-    case '見': return 'k';
-    case '溪': return 'kh';
-    case '羣': return 'g';
-    case '疑': return 'ng';
-    case '影': return 'q';
-    case '曉': return 'h';
-    case '匣': return 'gh';
-    case '云': return '';
-    case '以': return is脣化 ? 'y' : 'i';
-    case '來': return 'l';
-    default: throw new Error('無聲母規則');
+  let 聲母 = {
+    幫: 'p',   滂: 'ph',   並: 'b',   明: 'm',
+    端: 't',   透: 'th',   定: 'd',   泥: 'n',  來: 'l',
+    知: 'tr',  徹: 'thr',  澄: 'dr',  孃: 'nr',
+    見: 'k',   溪: 'kh',   羣: 'g',   疑: 'ng', 云: '',
+    影: 'q',   曉: 'h',    匣: 'gh', 
+    精: 'ts',  清: 'tsh',  從: 'dz',  心: 's',  邪: 'z',
+    莊: 'tsr', 初: 'tshr', 崇: 'dzr', 生: 'sr', 俟: 'zr',
+    章: 'tj',  昌: 'thj',  常: 'dj',  書: 'hj', 船: 'ghj', 日: 'nj', 以: 'i',
+  }[音韻地位.母];
+  // 章組自帶開合口信息
+  return (is圓脣) ? 聲母.replace('j', 'y').replace('i', 'y') : 聲母;
+}
+
+function get韻() {
+  // 爲了方便推導，對韻類稍作調整
+  if (音韻地位.描述 === '影開三蒸入' && '抑𡊁𢬃𡊶'.includes(字頭) ||
+      音韻地位.描述 === '溪開三蒸平') {
+    return '冰'; // 抑、硱小韻歸B類
   }
+  return 音韻地位.判斷([
+    ['蒸韻 幫組 或 蒸韻 合口', '冰'],
+    ['東韻 三等', '終'],
+    ['清韻', '庚'],
+    ['陽韻', '唐'],
+    ['莊組 臻欣韻', '眞'],
+    ['嚴凡韻 幫組', '凡'],
+    ['嚴凡韻', '嚴'],
+    ['', 音韻地位.韻],
+  ]);
 }
 
-function get介音() {
-  if (is('云母 灰韻'))               return 'v';
-  if (is('侯韻') || is('東韻 一等')) return 'o';
-  if (is('一四等'))                  return is('合口') ? 'w' : '';
-  if (is('二等') || is('庚韻 莊組')) return is('合口') ? 'rw' : 'r';
-  if (is三B)                         return is('合口') ? 'ry' : 'ri';
-  if (is('重紐A類 或 麻清幽韻'))     return is脣化 ? 'y' : 'i';
-  return is脣化 || (is('幫組') && !is('支宵侵韻')) ? 'u' : 'v';
-}
+function get韻母() {
+  const 韻到韻尾 = [
+    ['脂之尤侯 　佳　模　 支魚虞 麻歌', ''],
+    ['冰蒸終東 青耕登江冬 　　鍾 庚唐', 'ng', 'k'],
+    ['　微微　 齊皆咍灰　 祭廢廢 夬泰', 'i'],
+    ['眞欣文　 先山痕魂　 仙元元 刪寒', 'n', 't'],
+    ['幽　　　 蕭　　　　 宵　　 肴豪', 'w'],
+    ['侵　　　 添咸　覃　 鹽嚴凡 銜談', 'm', 'p'],
+  ];
+  const 元音列表 = [
+    'i',       'v',  'u', 'ou',
+    'e', 'e',  'o',  'o', 'ow',
+    'e',       'vo', 'uo',
+         'ae', 'a',
+  ];
 
-function get韻核() {
-  if (is('之蒸侵欣微韻'))           return is脣化 && !is('蒸韻') ? 'u' : 'v';
-  if (is('脂眞臻幽韻'))             return 'i';
-  if (is('尤侯東文韻'))             return 'u';
-  if (is('冬韻'))                   return 'ow'; 
-  if (is('模登覃痕魂灰咍韻') ||
-      is('魚虞鍾江嚴凡元廢韻'))     return 'o';
-  if (is('青添先蕭齊韻') ||
-      is('佳支耕鹽咸山仙宵皆祭韻')) return 'e';
-  if (is('麻庚清銜刪肴夬韻'))       return 'ae';
-  if (is('歌唐陽談寒豪泰韻'))       return 'a'; 
-  throw new Error('無韻核規則');
-}
+  let 韻 = get韻();
+  let 元音;
+  let 韻尾;
+  韻到韻尾.some((item) => {
+    if (item[0].includes(韻)) {
+      元音 = 元音列表[item[0].replace(/ /g, '')[is('開口') ? 'indexOf' : 'lastIndexOf'](韻)];
+      韻尾 = item[1 + is('入聲')];
+      return true;
+      }
+    });
 
-function get韻尾() {
-  if (is('梗曾攝'))       return is('入聲') ? 'k' : 'ng';
-  if (is('通江宕攝'))     return is('入聲') ? 'k' : 'ng';
-  if (is('深咸攝'))       return is('入聲') ? 'p' : 'm';
-  if (is('臻山攝'))       return is('入聲') ? 't' : 'n';
-  if (is('佳韻'))         return ''; // 從蟹攝中排除無韻尾的佳韻
-  if (is('微韻 或 蟹攝')) return 'i';
-  if (is('幽韻 或 效攝')) return 'w';
-  return '';
-}
+  if (is('三等') && !is非三三等) {
+    // 添加三等C介音（僅歌陽韻需要處理）
+    if (元音 === 'a') {
+      元音 = (is('開口') ? 'v' : 'u') + 元音;
+    }
 
-const 前元音 = ['i', 'e', 'ae'];
+    if (is強制三A && ['vo', 'uo', 'va', 'ua'].includes(元音)) {
+      元音 = 元音.replace('v', '').replace('u', '');
+    }
+    // 添加三等A、B介音
+    if (['i', 'e', 'ae'].includes(元音)) {
+      if (is('重紐B類 或 云母 或 知莊組 或 庚蒸韻 或 幽韻 幫組') ||
+        音韻地位.描述 === '曉三幽平' && '烋休𠇾'.includes(字頭) ||
+        音韻地位.描述 === '溪三幽平') { // 烋、𠁫小韻歸 B 類
+        if (選項.支B寫作C類 && is('支韻') && !is翹舌聲母) { 元音 = (is('合口') ? 'u' : 'v') + 元音; }
+        else if (選項.侵B寫作C類 && is('侵韻') && !is翹舌聲母) { 元音 = 'v'; }
+        else if (選項.宵B寫作C類 && is('宵韻') && !is翹舌聲母) { 元音 = 'v' + 元音; }
+        else { 元音 = (is('合口') ? 'ry' : 'ri') + 元音; }
+      } else if (!is強制三A) {
+        // 拼莊組以外的銳音一律視爲A類（同《切韻》清韻、《廣韻》諄韻的獨立條件）
+        // 章組和日以母已包含開合口及三等A類語義，故予以排除
+        元音 = (is圓脣 ? 'y' : 'i') + 元音;
+      }
+    }
+  }
+
+  // 添加合口介音
+  if (is('合口') && !元音.includes('u') && !元音.includes('y') && !is強制三A) 元音 = 'w' + 元音;
+  // 添加二等介音
+  if (is('二等')) 元音 = 'r' + 元音;
+  // 清理連續重複的字母
+  元音 = 元音.replace('ii', 'i');
+
+  if (選項.蒸韻韻核視作i && is('蒸韻') && !元音.includes('r')) 元音 = 'i';
+  
+  return 元音 + 韻尾;
+}
 
 function get聲調() {
-  if (is('平聲')) return '';
-  if (is('上聲')) return 'q';
-  if (is('去聲')) return 'h';
-  if (is('入聲')) return '';
-  throw new Error('無聲調規則');
+  return { 上: 'q', 去: 'h' }[音韻地位.聲] || '';
 }
 
-let 聲母 = get聲母();
-let 隔音符號 = is('曉母 三等 重紐A類 合口') ? '\'' : '';
-let 介音 = get介音();
-let 韻核 = get韻核();
-let 韻尾 = get韻尾();
-let 聲調 = get聲調();
+let result = get聲母() + (is('曉母 重紐A類 合口') ? '\'' : '') + get韻母() + get聲調();
 
-/**
- * 音韻調整
- */
+// 清理連續重複的字母
+result = result.replace('ii', 'i');
+result = result.replace('yy', 'y');
+result = result.replace('rr', 'r');
 
-if ((介音 == 'i' || 聲母.endsWith('j') || is銳) && 韻核 == 'v' && (韻尾 == 'm' || 韻尾 == 'n' || 韻尾 == 'p' || 韻尾 == 't'))
-  韻核 = 'i';
-
-if (is銳 && 前元音.includes(韻核)) {
-  if (介音 == 'v') 介音 = 'i';
-  if (介音 == 'u') 介音 = 'y';
-}
-
-if (介音 == 'v' && 'ui'.includes(韻核)) 介音 = '';
-
-if (介音 == 'u' && 韻核 == 'i') 介音 = 'y';
-
-if (介音 == 'u' && 韻核 == 'v' && !is('蒸韻')) 韻核 = 'u';
-
-if (聲母.endsWith('r')) {
-  if (介音 == 'v') 介音 = 'i';
-  if (介音 == 'u') 介音 = 'y';
-}
-
-/**
- * 拼式簡化
- */
-
-if (介音 == 韻核) 介音 = '';
-
-if (聲母 == 介音) 介音 = '';
-
-if (聲母.endsWith('r') && 介音.startsWith('r'))
-  介音 = 介音.replace('r', '');
-
-if (聲母.endsWith('i') || 聲母.endsWith('j') && 'vi'.includes(介音))
-  介音 = '';
-
-if (聲母.endsWith('y') && 'uy'.includes(介音))
-  介音 = '';
-
-if (韻核 == 'u' && 韻尾.startsWith('w'))
-  韻尾 = 韻尾.replace('w', '');
-
-if (介音.endsWith('i') && 韻核 == 'i')
-  介音 = 介音.replace('i', '');
-
-if (聲母 == 'i' && 韻核 == 'u') 聲母 = 'y';
-
-if ('uv'.includes(韻核)) {
-  if (介音.includes('i')) 介音 = 介音.replace('i', '');
-  if (介音.includes('y') && 韻核 == 'u') 介音 = 介音.replace('y', '');
-}
-
-/**
- * 處理特例
- */
-
-if (字頭 == '打' && is('庚韻')) {
-  聲母 = 't';
-  介音 = '';
-}
-
-if (音韻地位.描述 == '來開二庚上') 介音 = '';
-
-let result = 聲母 + 隔音符號 + 介音 + 韻核 + 韻尾 + 聲調;
-
-return result.replace('ii', 'i');
+return result;
